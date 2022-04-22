9da93e0af19d15c3e2240a2599870b7e6bba0e82
Jin0331 <jinoo@wmbio.co>
2022-04-22 09:57:08 +0900

    Update"

--- src/run_WCGNA_TCGA.R
+++ src/run_WCGNA_TCGA.R
@@ -80,17 +80,8 @@ run_deseq_normal <- function(pr_name, rdata_path, deg_path, batch_removal){
       robustdeg_ge <-  robustdeg_ge[gsg$goodSamples, gsg$goodGenes]
     }
     
-    # plot
-    sampleTree <-  hclust(dist(robustdeg_ge), method = "average");
-    # Plot the sample tree: Open a graphic output window of size 12 by 9 inches
-    # The user should change the dimensions if the window is too large or too small.
-    sizeGrWindow(12,9)
-    #pdf(file = "Plots/sampleClustering.pdf", width = 12, height = 9);
-    par(cex = 0.6);
-    par(mar = c(0,4,2,0))
-    plot(sampleTree, main = "Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5,
-         cex.axis = 1.5, cex.main = 2)
-    
+
+
     # power calculation
     powers <- c(c(1:10), seq(from = 12, to=20, by=2))
     sft <- pickSoftThreshold(robustdeg_ge, powerVector = powers, verbose = 5)
@@ -109,38 +100,32 @@ run_deseq_normal <- function(pr_name, rdata_path, deg_path, batch_removal){
                             saveTOMFileBase = "TCGA-LIHC",
                             verbose = 3)
     
-    # plot
-    {
-      sizeGrWindow(12, 9)
-      # Convert labels to colors for plotting
-      mergedColors <- labels2colors(net$colors)
-      # Plot the dendrogram and the module colors underneath
-      plotDendroAndColors(net$dendrograms[[1]], mergedColors[net$blockGenes[[1]]],
-                          "Module colors",
-                          dendroLabels = FALSE, hang = 0.03,
-                          addGuide = TRUE, guideHang = 0.05)
-    }
-    
     moduleLabels <- net$colors
     moduleColors <-  labels2colors(net$colors)
     MEs <- net$MEs;
     geneTree <- net$dendrograms[[1]]
     
     # clinical trait
+    # TCGAbiolinks clinical data
     # clinical_trait <- GDCquery_clinic(project = paste0("TCGA-", pr_name), type = "clinical")
-    # expression_sample <- rownames(robustdeg_ge) %>% as_tibble() %>% 
-    #   separate(col = value, into = c("A","B","C")) %>% 
-    #   unite(col = id, sep = "-") %>% pull(1)
+    
+    # UCSCXena clinical
     clinical_trait <- read_delim("https://tcga-xena-hub.s3.us-east-1.amazonaws.com/download/TCGA.LIHC.sampleMap%2FLIHC_clinicalMatrix",
                                  delim = "\t")
-    write_delim(clinical_trait, file = "TCGA-LIHC_Clinical_TCGAibolinks.txt", delim = "\t")
+    survival_trait <- read_delim("https://tcga-xena-hub.s3.us-east-1.amazonaws.com/download/survival%2FLIHC_survival.txt",
+                                 delim = "\t")
+    
+    clinical_trait <- left_join(x = clinical_trait, y = survival_trait, by = c("sampleID" = "sample"))
+    
+    # write_delim(clinical_trait, file = "TCGA-LIHC_Clinical_TCGAibolinks.txt", delim = "\t")
     
     expression_sample <- rownames(robustdeg_ge)
     traitRows <- match(expression_sample, clinical_trait$sampleID)
     data_trait <- clinical_trait[traitRows, ] %>% 
       column_to_rownames(var = "sampleID") %>% 
-      select(sample_type, age_at_initial_pathologic_diagnosis, pathologic_T, pathologic_M, pathologic_N,
-             pathologic_stage, child_pugh_classification_grade, fibrosis_ishak_score) %>% 
+      select(sample_type, OS, DSS, DFI, PFI, age_at_initial_pathologic_diagnosis, 
+             pathologic_T, pathologic_M, pathologic_N, pathologic_stage, child_pugh_classification_grade, 
+             fibrosis_ishak_score) %>% 
       mutate_all(as.factor) %>% 
       mutate_all(as.numeric)
     data_trait[is.na(data_trait)] <- 0
@@ -153,33 +138,59 @@ run_deseq_normal <- function(pr_name, rdata_path, deg_path, batch_removal){
     MEs0 <- moduleEigengenes(robustdeg_ge, moduleColors)$eigengenes
     MEs <- orderMEs(MEs0)
     
-    
     # trait cor
     moduleTraitCor <-  WGCNA::cor(MEs, data_trait, use = "p")
     moduleTraitPvalue <-  corPvalueStudent(moduleTraitCor, nSamples)
     
-    # plot
+    
+    # collection of plot
     {
-      sizeGrWindow(10,6)
-      # Will display correlations and their p-values
-      textMatrix = paste(signif(moduleTraitCor, 2), "\n(",
-                         signif(moduleTraitPvalue, 1), ")", sep = "");
-      dim(textMatrix) = dim(moduleTraitCor)
-      par(mar = c(6, 8.5, 3, 3));
-      # Display the correlation values within a heatmap plot
-      labeledHeatmap(Matrix = moduleTraitCor,
-                     xLabels = names(data_trait),
-                     yLabels = names(MEs),
-                     ySymbols = names(MEs),
-                     colorLabels = FALSE,
-                     colors = greenWhiteRed(50),
-                     textMatrix = textMatrix,
-                     setStdMargins = FALSE,
-                     cex.text = 0.5,
-                     zlim = c(-1,1),
-                     main = paste("Module-trait relationships"))
+      # plot - sample cluster
+      {
+        sampleTree <-  hclust(dist(robustdeg_ge), method = "average");
+        # Plot the sample tree: Open a graphic output window of size 12 by 9 inches
+        # The user should change the dimensions if the window is too large or too small.
+        sizeGrWindow(12,9)
+        #pdf(file = "Plots/sampleClustering.pdf", width = 12, height = 9);
+        par(cex = 0.6);
+        par(mar = c(0,4,2,0))
+        plot(sampleTree, main = "Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5,
+             cex.axis = 1.5, cex.main = 2)
+      }
+      # plot - module cluster
+      {
+        sizeGrWindow(12, 9)
+        # Convert labels to colors for plotting
+        mergedColors <- labels2colors(net$colors)
+        # Plot the dendrogram and the module colors underneath
+        plotDendroAndColors(net$dendrograms[[1]], mergedColors[net$blockGenes[[1]]],
+                            "Module colors",
+                            dendroLabels = FALSE, hang = 0.03,
+                            addGuide = TRUE, guideHang = 0.05)
+      }
+      # plot - module-trait relationships
+      {
+        sizeGrWindow(10,6)
+        # Will display correlations and their p-values
+        textMatrix = paste(signif(moduleTraitCor, 2), "\n(",
+                           signif(moduleTraitPvalue, 1), ")", sep = "");
+        dim(textMatrix) = dim(moduleTraitCor)
+        par(mar = c(6, 8.5, 3, 3));
+        # Display the correlation values within a heatmap plot
+        labeledHeatmap(Matrix = moduleTraitCor,
+                       xLabels = names(data_trait),
+                       yLabels = names(MEs),
+                       ySymbols = names(MEs),
+                       colorLabels = FALSE,
+                       colors = greenWhiteRed(50),
+                       textMatrix = textMatrix,
+                       setStdMargins = FALSE,
+                       cex.text = 0.5,
+                       zlim = c(-1,1),
+                       main = paste("Module-trait relationships"))
+      }
+      
     }
-
   })  
   
   return(tcga_deseq_result_tidy)
